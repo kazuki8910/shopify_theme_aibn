559461433547a8e494de8650ed349313526936d7
*q91fTG8Von <kumagikazu@gmail.com>
2023-04-01 19:46:39 +0900

    完了

---
+++ assets/add.js
@@ -0,0 +1,29 @@
+$(function(){
+ 
+
+  // ヘッダーメニューの開閉
+  {
+    const $btn = $('.add_header_btn');
+
+    $btn.click(function(){
+      const index = $btn.index(this);
+      const $menus = $('.add_header_menu');
+      const $this_menu = $menus.eq(index);
+
+      // メニューを閉じる
+      if($(this).hasClass('add_header_btn_active')){
+        $(this).removeClass('add_header_btn_active');  
+        $this_menu.fadeOut(200);
+      }
+      // メニューを開く
+      else{
+        $btn.removeClass('add_header_btn_active');
+        $(this).addClass('add_header_btn_active');
+        $menus.fadeOut(200);
+        $this_menu.fadeIn(200);
+      }
+    });
+  }
+ 
+});
+
